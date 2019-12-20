a01ff7da6fb49d036c8d2947dda442072aa5f542
Theodorus Yoga <theodorus@keyreply.com>
2019-12-20 12:09:54 +0700

    Added transpiler dependencies

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const merge = require('webpack-merge');
 module.exports = {
   // publicPath:'/wfd-vue/',
+  transpileDependencies: ['@antv/g6', '@antv/util'],
   productionSourceMap: false,
   configureWebpack: config => {
     if(process.env.NODE_ENV === 'production') {
